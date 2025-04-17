b795c15d28e99f75a3117918c3c529aa4895916d
Damilola <59915965+damiola-art@users.noreply.github.com>
2025-04-17 14:12:46 +0100

    Update base URL to match repository name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-portifolo/',  // Add this line - use your repo name
   assetsInclude: ["**/*.JPG", "**/*.jpg", "**/*.png"],
 })
