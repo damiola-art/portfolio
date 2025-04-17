2e608dfef54e4276ab67e8050025766fcc7548a5
Damilola <59915965+damiola-art@users.noreply.github.com>
2025-04-17 13:21:27 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,81 @@
+import project1 from "../assets/projects/project22.png";
+import project2 from "../assets/projects/project1.png";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate Full Stack Developer with a knack for crafting robust and scalable web applications. With years of hands-on experience, I have honed my skills in front-end technologies such as JavaScript and React, and back-end technologies including Node.js, MySQL, PostgreSQL, and MongoDB. I bring an advanced understanding of agile methodologies, including Continuous Integration/Continuous Deployment (CI/CD), application resiliency, and security best practices. `;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Software Developer",
+    company: "Elixonn Healthcare ltd.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2020 - 2022",
+    role: "Software Engineer",
+    company: "DHL Africa",    
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2017 - 2020",
+    role: "Full Stack Developer",
+    company: "Allied Computer Ltd",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2014  - 2015",
+    role: "Techinal Support Engineer Internship",
+    company: "CBC EMEA",
+    description: `Provide onsite and desk side support services to end users.
+Complete incidents and requests within SLA in a pressurized environment.
+Build, configure and troubleshoot Install applications on Customer PC and laptop and hardware components.
+Ensure tickets are updated on a daily basis.
+End-User Data back-up and restoration when required in the course of a service incident.
+Perform hardware and software upgrades on desktops and laptops.`,
+    technologies: ["ServiceNow", "Active directory", "SCCM", "MDT" ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Comprehensive Real Estate Price Comparison Platform for Renting                                                              ",
+    image: project1,
+    description:
+      "The goal of this project is to develop a sophisticated web-based platform that enables users to compare property prices across various real estate websites. By integrating real estate market analysis with advanced information technology, including automated web scraping .",
+    technologies: ["HTML", "CSS", "React", "sql", "python", "Express.js"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project2,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information..",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Task Management App",
+    image: project3,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "london, United Kingdom",
+  phoneNo: "+7377370240 ",
+  email: "djinadu7@gmail.com",
+};
